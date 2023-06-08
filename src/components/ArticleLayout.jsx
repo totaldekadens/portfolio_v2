715bf3d19ce076f179d27bcd03b3327eeea45439
totaldekadens <angelica.moberg@hotmail.com>
2023-06-08 12:04:05 +0200

    fix button

--- src/components/ArticleLayout.jsx
+++ src/components/ArticleLayout.jsx
@@ -46,7 +46,7 @@ export function ArticleLayout({
                   type="button"
                   onClick={() => router.back()}
                   aria-label="Go back to projects"
-                  className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition  dark:bg-dark-300 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20 lg:absolute lg:-left-5 lg:mb-0 lg:-mt-2 xl:-top-1.5 xl:left-0 xl:mt-0"
+                  className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition  dark:bg-light-300/10 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20 lg:absolute lg:-left-5 lg:mb-0 lg:-mt-2 xl:-top-1.5 xl:left-0 xl:mt-0"
                 >
                   <ArrowLeftIcon className="h-4 w-4 stroke-zinc-500 transition group-hover:stroke-zinc-700 dark:stroke-light-200 dark:group-hover:stroke-zinc-400" />
                 </button>
